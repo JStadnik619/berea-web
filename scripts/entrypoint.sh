cf23fbd772075e152895c6f44dbc342c4dc8c9a5
Jamie Stadnik <JStadnik619@gmail.com>
2025-10-03 10:22:37 -0500

    Display request logs on stdout

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# BUG: Site can't be reached inside docker container
-# TODO: Set logfile with --access-logfile=
-gunicorn -w 4 --bind 0.0.0.0:80 'app:app'
+gunicorn -w 4 --bind 0.0.0.0:80 'app:app' --access-logfile=-
