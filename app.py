6e9fc7e12ac474d2846fedfd330d8d3677ac166d
Jamie Stadnik <JStadnik619@gmail.com>
2025-11-17 08:44:09 -0600

    Remove FTS input (permanently enabled)

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def search():
     book = request.args.get('book')
     chapter = request.args.get('chapter')
     testament = request.args.get('testament')
-    fts = request.args.get('full-text')
     
     bible = BibleClient(translation)
     
@@ -30,18 +29,18 @@ def search():
     
     # TODO: Validate inputs/input combinations
     if book and chapter:
-        verse_records = bible.search_chapter(phrase, book, chapter, fts)
+        verse_records = bible.search_chapter(phrase, book, chapter)
         book_name = bible.get_book_from_abbreviation(book)
         results_description = f"{len(verse_records)} occurrences of '{phrase}' in {book_name} {chapter} ({translation})"
     elif book:
-        verse_records = bible.search_book(phrase, book, fts)
+        verse_records = bible.search_book(phrase, book)
         book_name = bible.get_book_from_abbreviation(book)
         results_description = f"{len(verse_records)} occurrences of '{phrase}' in {book_name} ({translation})"
     elif testament in ['New Testament', 'Old Testament']:
-        verse_records = bible.search_testament(phrase, 'nt' if testament == 'New Testament' else 'ot', fts)
+        verse_records = bible.search_testament(phrase, 'nt' if testament == 'New Testament' else 'ot')
         results_description = f"{len(verse_records)} occurrences of '{phrase}' in the {testament} ({translation})"
     else:
-        verse_records = bible.search_bible(phrase, fts)
+        verse_records = bible.search_bible(phrase)
         results_description = f"{len(verse_records)} occurrences of '{phrase}' in the {translation} Bible"
     
     context = {
