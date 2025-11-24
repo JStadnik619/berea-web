1fb23e7185c5f39b4a9dfd9c7429c669d05450a3
Jamie Stadnik <JStadnik619@gmail.com>
2025-11-24 15:25:25 -0600

    Handle search exceptions

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+import logging
+from textwrap import dedent
+
 from flask import Flask, render_template, request
-from berea.bible import BibleClient
+from berea.bible import BibleClient, BibleInputError
+
+
+logger = logging.getLogger(__name__)
 
 
 app = Flask(__name__)
@@ -27,21 +33,38 @@ def search():
     verse_records = []
     results_description = ""
     
-    # TODO: Validate inputs/input combinations
-    if book and chapter:
-        verse_records = bible.search_chapter(phrase, book, chapter)
-        book_name = bible.get_book_from_abbreviation(book)
-        results_description = f"{len(verse_records)} occurrences of '{phrase}' in {book_name} {chapter} ({translation})"
-    elif book:
-        verse_records = bible.search_book(phrase, book)
-        book_name = bible.get_book_from_abbreviation(book)
-        results_description = f"{len(verse_records)} occurrences of '{phrase}' in {book_name} ({translation})"
-    elif testament in ['New Testament', 'Old Testament']:
-        verse_records = bible.search_testament(phrase, 'nt' if testament == 'New Testament' else 'ot')
-        results_description = f"{len(verse_records)} occurrences of '{phrase}' in the {testament} ({translation})"
-    else:
-        verse_records = bible.search_bible(phrase)
-        results_description = f"{len(verse_records)} occurrences of '{phrase}' in the {translation} Bible"
+    try:
+        
+        # TODO: Validate inputs/input combinations
+        if book and chapter:
+            verse_records = bible.search_chapter(phrase, book, chapter)
+            book_name = bible.get_book_from_abbreviation(book)
+            results_description = f"{len(verse_records)} occurrences of '{phrase}' in {book_name} {chapter} ({translation})"
+        elif book:
+            verse_records = bible.search_book(phrase, book)
+            book_name = bible.get_book_from_abbreviation(book)
+            results_description = f"{len(verse_records)} occurrences of '{phrase}' in {book_name} ({translation})"
+        elif testament in ['New Testament', 'Old Testament']:
+            verse_records = bible.search_testament(phrase, 'nt' if testament == 'New Testament' else 'ot')
+            results_description = f"{len(verse_records)} occurrences of '{phrase}' in the {testament} ({translation})"
+        else:
+            verse_records = bible.search_bible(phrase)
+            results_description = f"{len(verse_records)} occurrences of '{phrase}' in the {translation} Bible"
+    
+    except BibleInputError as bible_ex:
+        return render_template('search_error.html', error_msg=str(bible_ex))
+    
+    except Exception as ex:
+        input_msg = dedent(f"""
+            User provided invalid search input:
+            {phrase=}
+            {book=}
+            {chapter=}
+            {testament=}\n
+        """)
+        logger.exception(input_msg)
+        logger.exception(ex)
+        return render_template('search_error.html', error_msg='Invalid input.')
     
     context = {
         'translation': translation,
